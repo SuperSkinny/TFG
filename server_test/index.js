c83b9175dc0d4eafa50e980c3ff957264420c371
Pedro <3892436@alu.murciaeduca.es>
2021-04-30 14:29:26 +0200

    Index del area de pruebas cambiado

--- server_test/index.js
+++ server_test/index.js
@@ -2,5 +2,9 @@ const Model = require('./models/model')
 
 // Area de pruebas
 
-console.log('a')
-Model.getAllScoresOfACategory('faciles')
+console.log('Area pruebas')
+// Model.checkIfEmailExists('pedrin@quizroyale.com')
+Model.getAllScoresOfUser('hl8iyxzgRi')
+.then(response => {
+    console.log(response)
+})
