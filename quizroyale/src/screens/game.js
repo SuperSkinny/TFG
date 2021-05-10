6da48127c2e0a54c518ab4f1c870989dce2f690f
Pedro <3892436@alu.murciaeduca.es>
2021-05-10 23:48:08 +0200

    Ahora coge una pregunta aleatoria del array

--- quizroyale/src/screens/game.js
+++ quizroyale/src/screens/game.js
@@ -16,17 +16,41 @@ export default class Game extends Component {
   async componentDidMount() {
     this.setState({questions_answers: await model.getAllQuestionsAndAnswersByCategory(this.props.gameModeName)})
   }
+
+  logicForQuestionsAndAnswers(questions_answers) {
+    var randomQuestion = Math.floor(Math.random() * questions_answers.length)
+    // let questions = questions_answers.map(x => Object.keys(x)[0])
+
+    // console.log(questions_answers[randomQuestion])
+
+    // var questionAndAnswers = {}
+    // questionAndAnswers[questions[randomQuestion]] = questions_answers[randomQuestion]
+    // console.log('Pregunta y respuestas obtenidas: ', questionAndAnswers)
   
+    // console.log('? ', questions_answers[randomQuestion])
+    // questions_answers.splice(randomQuestion, 1)
+    // console.log('Array sin esa pregunta: ', questions_answers)
+    return questions_answers[randomQuestion]
+
+  }
+
   render() {
     const { gameModeName, onGameGoBack } = this.props;
     const { questions_answers } = this.state;
-    console.log("GameMode en game Component: " + gameModeName)
-    console.log('Preguntas y respuestas: ' + questions_answers)
+
+    var questionAndAnswers
+
+    if (questions_answers.length !== 0) {
+      questionAndAnswers = this.logicForQuestionsAndAnswers(questions_answers)
+    }
+
+    // console.log("GameMode en game Component: " + gameModeName)
+    // console.log('Preguntas y respuestas: ', questions_answers[1])
 
     return (
       <React.Fragment>
         <GameComponent
-          gameModeQuestion={questions_answers}
+          gameModeQuestion={questionAndAnswers}
           // TODO: hay que resetear el juego aquí cuando salimos
           onGameGoBack={onGameGoBack}
         />
