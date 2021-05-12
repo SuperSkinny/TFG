7b78bf9daf5db31664a1646f1b4b4410f45ee9a1
Pedro <3892436@alu.murciaeduca.es>
2021-05-12 20:23:05 +0200

    Al clicar una respuesta se le pasa al padre

--- quizroyale/src/components/gameComponent.js
+++ quizroyale/src/components/gameComponent.js
@@ -4,65 +4,69 @@ import { Link } from "react-router-dom";
 
 export default class GameComponent extends Component {
 
-  render() {
-    const { gameModeQuestion, onGameGoBack } = this.props;
-    console.log(gameModeQuestion)
-    if (!gameModeQuestion){
-        return null;
+    render() {
+        const { gameModeQuestion, onGameGoBack, onResponsePress } = this.props;
+        console.log(gameModeQuestion)
+        if (!gameModeQuestion){
+            return null;
+        }
+
+        // const { question } = gameModeQuestion;
+        
+        // let answerStyle = false;
+
+        return (
+            <div className="content" style={ { display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center" }}>
+                <div>
+                    <span className="generalTitle" >
+                        {gameModeQuestion.question}
+                    </span>
+                </div>
+                <div  style={ { display: "flex", justifyContent: 'center', flexWrap: "wrap" }}>
+                    <button 
+                        onClick={ ()=> {
+                            onResponsePress(gameModeQuestion.answer1.correct, gameModeQuestion.question)
+                        }}
+                        className="gameButton"
+                        // style={ !answerStyle ? { backgroundColor: "red" } : { backgroundColor: "green" }}
+                    >
+                        {gameModeQuestion.answer1.answer} 
+                    </button>
+                    <button
+                        onClick={ ()=> {
+                            onResponsePress(gameModeQuestion.answer2.correct, gameModeQuestion.question)
+                        }} 
+                        className="gameButton" 
+                    >
+                        {gameModeQuestion.answer2.answer}
+                    </button>
+                    <button
+                        onClick={ ()=> {
+                            onResponsePress(gameModeQuestion.answer3.correct, gameModeQuestion.question)
+                        }} 
+                        className="gameButton" 
+                    >
+                        {gameModeQuestion.answer3.answer}
+                    </button>
+                </div>
+                
+                <div className="progressBarContainer">
+                    <div className="progressBar" style={{ width: "25%" }} ></div>
+                </div>
+                <div style={ { marginTop: 30 } }>
+                    <button 
+                        type="button" 
+                        className="generalButton"
+                        onClick={ () => {
+                            // TODO: hay que resetear el juego aquí cuando salimos
+                            //console.log("SALIR A PREGAME")
+                            onGameGoBack()
+                        }}
+                    >
+                        Salir
+                    </button>
+                </div>
+            </div>
+        );
     }
-
-    // const { question } = gameModeQuestion;
-    
-    // let answerStyle = false;
-
-    // const handleResponse = (answer) => {
-    //     if (answer === true) {
-    //         this.answerStyle = true;
-    //     }
-    // }
-
-    return (
-        <div className="content" style={ { display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center" }}>
-            <div>
-                <span className="generalTitle" >
-                    {gameModeQuestion.question}
-                </span>
-            </div>
-            <div  style={ { display: "flex", justifyContent: 'center', flexWrap: "wrap" }}>
-                <button 
-                    onClick={ ()=> {
-                        // handleResponse(answer1.valor)
-                    }}
-                    className="gameButton"
-                    // style={ !answerStyle ? { backgroundColor: "red" } : { backgroundColor: "green" }}
-                >
-                    {gameModeQuestion.answer1.answer} 
-                </button>
-                <button className="gameButton" >
-                    {gameModeQuestion.answer2.answer}
-                </button>
-                <button type="button"  className="gameButton">
-                    {gameModeQuestion.answer3.answer}
-                </button>
-            </div>
-            
-            <div className="progressBarContainer">
-                <div className="progressBar" style={{ width: "25%" }} ></div>
-            </div>
-            <div style={ { marginTop: 30 } }>
-                <button 
-                    type="button" 
-                    className="generalButton"
-                    onClick={ () => {
-                        // TODO: hay que resetear el juego aquí cuando salimos
-                        //console.log("SALIR A PREGAME")
-                        onGameGoBack()
-                    }}
-                >
-                    Salir
-                </button>
-            </div>
-        </div>
-    );
-  }
 }
