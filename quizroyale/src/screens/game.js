78dd239bd3f6395c03e2bb1440814d32f193d58d
Pedro <3892436@alu.murciaeduca.es>
2021-05-12 20:22:25 +0200

    Se muestran todas las preguntas random y se borran

--- quizroyale/src/screens/game.js
+++ quizroyale/src/screens/game.js
@@ -9,51 +9,79 @@ export default class Game extends Component {
   constructor(props) {
     super(props);
     this.state = { 
-      questions_answers: [], 
+      questions_answers: [],
+      points: 0,
+      lifeBar: 100,
+      gameEnded: false,
     }
   }
-
+  
   async componentDidMount() {
+    console.log('Entro a did mount')
     this.setState({questions_answers: await model.getAllQuestionsAndAnswersByCategory(this.props.gameModeName)})
   }
-
-  logicForQuestionsAndAnswers(questions_answers) {
-    var randomQuestion = Math.floor(Math.random() * questions_answers.length)
-    // let questions = questions_answers.map(x => Object.keys(x)[0])
-
-    // console.log(questions_answers[randomQuestion])
-
-    // var questionAndAnswers = {}
-    // questionAndAnswers[questions[randomQuestion]] = questions_answers[randomQuestion]
-    // console.log('Pregunta y respuestas obtenidas: ', questionAndAnswers)
   
-    // console.log('? ', questions_answers[randomQuestion])
-    // questions_answers.splice(randomQuestion, 1)
-    // console.log('Array sin esa pregunta: ', questions_answers)
-    return questions_answers[randomQuestion]
+  // componentDidUpdate(prevProps) {
+  //   if(this.props.questions_answers !== prevProps.questions_answers) {
+  //     this.logicForQuestionsAndAnswers(prevProps.questions_answers)
+  //   }
+  // }
 
+  getRandomQuestionAndAnswers(questions_answers) {
+    var randomQuestion = Math.floor(Math.random() * questions_answers.length)
+
+    return questions_answers[randomQuestion]
+  }
+
+  handleResponse(answer, question) {
+    const { questions_answers } = this.state
+    this.questions_answers = questions_answers.filter(deleteRandomQuestion => deleteRandomQuestion.question !== question)
+
+    if (answer === true) {
+      this.setState({
+        questions_answers: this.questions_answers,
+        points: this.state.points + 1
+      })
+    } else {
+      this.setState({
+        questions_answers: this.questions_answers,
+        lifeBar: this.state.lifeBar - 25
+      })
+    }
   }
 
   render() {
-    const { gameModeName, onGameGoBack } = this.props;
-    const { questions_answers } = this.state;
+    const { onGameGoBack } = this.props;
+    const { questions_answers, points, gameEnded } = this.state;
 
-    var questionAndAnswers
-
-    if (questions_answers.length !== 0) {
-      questionAndAnswers = this.logicForQuestionsAndAnswers(questions_answers)
+    if(questions_answers.length === 0) {
+      console.log('Entrada a null')
+      return null
     }
+
+    const questionAndAnswers = this.getRandomQuestionAndAnswers(questions_answers)
 
     // console.log("GameMode en game Component: " + gameModeName)
     // console.log('Preguntas y respuestas: ', questions_answers[1])
 
     return (
       <React.Fragment>
-        <GameComponent
-          gameModeQuestion={questionAndAnswers}
-          // TODO: hay que resetear el juego aquí cuando salimos
-          onGameGoBack={onGameGoBack}
-        />
+        {!gameEnded ? (
+        <div>
+          <div>
+            Hola
+            {points}
+          </div>
+          <GameComponent
+            gameModeQuestion={questionAndAnswers}
+            // TODO: hay que resetear el juego aquí cuando salimos
+            onGameGoBack={onGameGoBack}
+            onResponsePress={ (answer, question) => this.handleResponse(answer, question)}
+          />
+        </div>
+        ) : (<div>Se ha acabado</div>) 
+        }
+        
       </React.Fragment>
     )
   }
