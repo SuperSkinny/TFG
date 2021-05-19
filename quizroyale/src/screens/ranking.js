5c2c1b163dd155b36bc5c56b9b7f2bf7a95b6323
Pedro <3892436@alu.murciaeduca.es>
2021-05-19 23:34:36 +0200

    Ahora obtiene las puntuaciones de las categorias

--- quizroyale/src/screens/ranking.js
+++ quizroyale/src/screens/ranking.js
@@ -1,9 +1,33 @@
 import React, { Component } from 'react';
 import RankingComponent from '../components/rankingComponent';
+import model from '../api/model';
 
 export default class Ranking extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = { 
+      novatoScores: [],
+      viciadoScores: [],
+      hackerScores: [],
+    }
+  }
+
+  async componentDidMount() {
+      this.setState({
+        novatoScores: await model.getBestThreeScoresOfACategory('Novato'),
+        viciadoScores: await model.getBestThreeScoresOfACategory('Viciado'),
+        hackerScores: await model.getBestThreeScoresOfACategory('Hacker'),
+      })
+  }
+
   render() {
+    const { novatoScores, viciadoScores, hackerScores } = this.state;
+
     return (
+      <div>
+        {!!novatoScores.length && !!viciadoScores.length && !!hackerScores.length && (
         <div  style={ { display: "flex", flexDirection: "column", alignItems: 'center' }}>
                 <span className="generalTitle" >
                     Ranking
@@ -11,16 +35,18 @@ export default class Ranking extends Component {
 
             <div style={ { display: "flex", flexWrap: "wrap", justifyContent: "center" }}>
                 <RankingComponent
-                    category={"Novato"}
+                    scores={novatoScores}
                 />
                 <RankingComponent
-                    category={"Viciado"}
+                    scores={viciadoScores}
                 />
                 <RankingComponent
-                    category={"Hacker"}
+                    scores={hackerScores}
                 />
             </div> 
         </div>
+      )}
+      </div>
     )
   }
 }
